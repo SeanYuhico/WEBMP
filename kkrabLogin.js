e49cd3f52dd9a08fd3a57672539107f584eaf8d0
shasha.v <patriciavillaroman@gmail.com>
2019-06-27 13:51:06 +0800

    kkrab can be "logged in"

---
+++ kkrabLogin.js
@@ -0,0 +1,23 @@
+// Place this at the bottom of the <input>s, unless you're running this after document is ready
+
+/*
+var btn = document.getElementsByClassName("submit-btn")
+
+btn.onclick=function(){
+  event.preventDefault();
+  console.log(document.getElementsByClassName("login-field_input").value)
+}
+
+*/
+function validateForm() {
+    event.preventDefault()
+    console.log("validate form")
+    console.log(document.getElementById("username").value)
+    console.log(document.getElementById("password").value)
+
+    if (document.getElementById("username").value == "admin"
+        && document.getElementById("password").value == "p@ssword") {
+            console.log("woh")
+            window.open('/kkrabFull.html', '_self');
+        }
+  }
