65948bcc23ae630623a30f227942d3faa680559d
sean.yuhico <sean.yuhico@gmail.com>
2019-06-30 21:04:16 +0800

    tangina nagloload na yung json

--- test3.js
+++ test3.js
@@ -4,20 +4,32 @@
 //         var canvas = d3.select(".")
 //     })
 // }
+let burgSales, specSales, burgSpec; //kPattie, kDeLuxe;
+    
 $(document).ready(function(){
     // $.getJSON("krustykrab.json",function(data){
+    //     burgSales = JSON.parse(data)
     //     console.log(data)
     // });
-
+    // burgSales = JSON.parse("krustykrabarray.json")
+    // console.log(burgSales)
     $.ajax({
         url: "krustykrab.json",
         dataType: "json",
         type: "get",
         cache: false,
         success: function(data){
-            $(data.species_sales).each(function(index, value){
-                console.log(value)
-            })
+            $(data).each(function(index, value){ //$(data.species_sales) is the specific object
+                console.log(data)
+                // console.log(burgSales)
+                burgSales = data.burger_sales
+                specSales = data.species_sales
+                burgSpec = data.burger_by_species
+                // kPattie = data.burger_sales
+                console.log(data.burger_sales);
+                console.log(specSales);
+                console.log(burgSpec);
+            });
         }
-    })
+    });
 });
