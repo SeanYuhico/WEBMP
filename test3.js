216b01835a242b3cc1367e739e26ae940810cd04
sean.yuhico <sean.yuhico@gmail.com>
2019-07-01 01:39:22 +0800

    don't mind the other shit that's here

--- test3.js
+++ test3.js
@@ -1,18 +1,6 @@
-// function loadData(){
-//     d3.json("krustykrab.json", function(data){
-//         console.log()
-//         var canvas = d3.select(".")
-//     })
-// }
-let burgSales, specSales, burgSpec; //kPattie, kDeLuxe;
+let burgSales={}, specSales={}, burgSpec={}; //kPattie, kDeLuxe;
     
 $(document).ready(function(){
-    // $.getJSON("krustykrab.json",function(data){
-    //     burgSales = JSON.parse(data)
-    //     console.log(data)
-    // });
-    // burgSales = JSON.parse("krustykrabarray.json")
-    // console.log(burgSales)
     $.ajax({
         url: "krustykrab.json",
         dataType: "json",
@@ -20,16 +8,51 @@ $(document).ready(function(){
         cache: false,
         success: function(data){
             $(data).each(function(index, value){ //$(data.species_sales) is the specific object
-                console.log(data)
+                // console.log(data)
                 // console.log(burgSales)
                 burgSales = data.burger_sales
                 specSales = data.species_sales
                 burgSpec = data.burger_by_species
                 // kPattie = data.burger_sales
-                console.log(data.burger_sales);
-                console.log(specSales);
-                console.log(burgSpec);
+                console.log(burgSales);
+                // console.log(specSales);
+                // console.log(burgSpec);
+                createGraph(burgSales);
             });
-        }
+            
+        } 
     });
 });
+
+function createGraph(data){
+    let sample = $(data).each(function(index, value){
+        
+    })
+    // console.log(sample)
+    console.log(sample);
+    console.log(val.size)
+    var svgWidth = 500, svgHeight = 300, barPadding = 5;
+    var barWidth = (svgWidth/val.value);
+    
+    var svg = d3.select('svg')
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+
+    var barChart = svg.selectAll("rect")
+        .data(value)
+        .enter()
+        .append("rect")
+        .attr("y", function(d){
+            return svgHeight - d;
+        })
+        .attr("height", function(d){
+            return d;
+        })
+        .attr("width", barWidth - barPadding)
+        .attr("transform", function(d, i){
+            var translate = [barWidth*i, 0];
+            return "translate("+ translate +")";
+        })
+
+    
+}
