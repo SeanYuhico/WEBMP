3e4997112c958652cfc435664658ef749f40a2cb
shasha.v <patriciavillaroman@gmail.com>
2019-07-10 22:18:00 +0800

    updated krab dashboard js

--- krab.js
+++ krab.js
@@ -12,10 +12,23 @@ $(document).ready(function(){
       console.log("fasdfa")
       console.log($(this).attr('href'))
       console.log($($(this).attr('href')).offset())
-      $('html, body').animate({
+      let target = $(this.getAttribute('href'));
+
+      console.log(target.offset())
+      
+
+      if( target.length ) {
+          event.preventDefault();
+          $("#data-visualization-slides").stop().animate({
+              scrollLeft: target.offset().left
+          }, 500);
+          
+      }
+
+      /*$('html, body').animate({
           scrollLeft: $($(this).attr('href')).offset().left
       }, 500);
-      return false;
+      return false;*/
   });
     
 });
