92cf97bbc9039a965e85c64acbc318d763b614d8
shasha.v <patriciavillaroman@gmail.com>
2019-07-10 09:26:10 +0800

    added filtering to salestables

--- datatables/salestable.js
+++ datatables/salestable.js
@@ -3,6 +3,7 @@ let dateTimeList=[];
 let timeList = []
 let dayIndexes = []
 let bilang = []
+let data = []
 
 function float2dollar(value) {
     return "U$ " + (value).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
@@ -28,65 +29,9 @@ function getTableData() {
         success: function (result) {
             console.log("pasok")
             $("#loadingMessage").html("");
-
-            let tempDate = "2011-07-14"
-
-            for(let key in result) {
-
-                let temp = result[key].datetime
-                let formatted = temp.substring(0,10);
-                tempDate = formatted;
-                let tempDateTime = temp.substring(11,19);
-                if(datesList.length===0){
-                    datesList.push(formatted);
-                    dayIndexes.push(new Array())
-                    dayIndexes[0].push(key)
-                    timeList.push(new Array())
-                    timeList[0].push(tempDateTime)
-                }
-                else{
-                    let isInList = false
-                    for(let i=0;i<datesList.length;i++){
-                        if(datesList[i] === tempDate){
-                            dayIndexes[i].push(key)
-                            timeList[i].push(tempDateTime)
-                            isInList = true
-                        }
-                        
-                    }
-
-                    if (!isInList) {
-                        datesList.push(formatted);
-                        dayIndexes.push(new Array())
-                        dayIndexes[dayIndexes.length - 1].push(key)
-                        timeList.push(new Array())
-                        timeList[timeList.length - 1].push(tempDateTime)
-                    }
-                }
-                // timeList.push(tempDateTime)
-                dateTimeList.push(temp);        
-            }
-            for(let i=0;i<dayIndexes.length;i++){
-                bilang.push(dayIndexes[i].length)
-            }
-
-            let dateIndex = -1;
-            for(let i=0;i<datesList.length;i++){
-                if(datesList[i] === tempDate){
-                    dateIndex = i;
-                    break;
-                }
-            }
-
-            let filteredSales = [];
-            for (k in dayIndexes[dateIndex]) {
-                filteredSales.push(result[k])
-            }
-            
-
-
-
-
+            data = result;
+            retrieveSalesIndexFilters(data)
+            let filteredSales = filterByDateTime("2019-06-07", "12")
             console.log(result)
             console.log(filteredSales)
             renderTable(filteredSales);
@@ -97,34 +42,78 @@ function getTableData() {
     });
 }
 
-function filterByDate(array, date){
-    let filtered = [];
-    filteredSalesCount = 0;
-    for(let i = 0; i < array.length; i++){
-
-        let obj = array[i];
-        let strDatetime = obj.datetime;
-        if (strDatetime.includes(date)) {
-            filtered.push(obj)
+function retrieveSalesIndexFilters (result) {
+    for (let key in result) {
+        let temp = result[key].datetime
+        let formatted = temp.substring(0,10);
+        tempDate = formatted;
+        let tempDateTime = temp.substring(11,19);
+        if(datesList.length===0){
+            datesList.push(formatted);
+            dayIndexes.push(new Array())
+            dayIndexes[0].push(key)
+            timeList.push(new Array())
+            timeList[0].push(tempDateTime)
         }
-    }    
-    filteredSalesCount = filtered.length;
-    return filtered;
+        else{
+            let isInList = false
+            for(let i=0;i<datesList.length;i++){
+                if(datesList[i] === tempDate){
+                    dayIndexes[i].push(key)
+                    timeList[i].push(tempDateTime)
+                    isInList = true
+                }
+            }
+
+            if (!isInList) {
+                datesList.push(formatted);
+                dayIndexes.push(new Array())
+                dayIndexes[dayIndexes.length - 1].push(key)
+                timeList.push(new Array())
+                timeList[timeList.length - 1].push(tempDateTime)
+            }
+        }
+        // timeList.push(tempDateTime)
+        dateTimeList.push(temp);        
+    }
+    for(let i=0;i<dayIndexes.length;i++){
+        bilang.push(dayIndexes[i].length)
+    }
 }
 
-function filterByDateTime(array, date, time){
-    var filtered = [];
-    filteredSalesCount = 0;
-    for(var i = 0; i < array.length; i++){
-
-        var obj = array[i];
-        let strDatetime = obj.datetime;
-        if (strDatetime.includes(date + " " + time)) {
-            filtered.push(obj)
+function filterByDate(dateInput){
+    let filteredSales = [];
+    let dateIndex = -1;
+    for(let i=0;i<datesList.length;i++){
+        if(datesList[i] === dateInput){
+            dateIndex = i;
+            break;
         }
-    }    
-    filteredSalesCount = filtered.length;
-    return filtered;
+    }
+
+    for (k in dayIndexes[dateIndex]) {
+        filteredSales.push(data[k])
+    }
+    return filteredSales;
+}
+
+function filterByDateTime(dateInput, timeInput){
+    let filteredSales = [];
+    let dateIndex = -1;
+    for(let i=0;i<datesList.length;i++){
+        if(datesList[i] === dateInput){
+            dateIndex = i;
+            break;
+        }
+    }
+    console.log(dayIndexes == null)
+
+    for (let i = 0; i < dayIndexes[dateIndex].length; i++) {
+        if (timeList[dateIndex][i].startsWith(timeInput)) {
+            filteredSales.push(data[dayIndexes[dateIndex][i]])
+        }
+    }
+    return filteredSales;
 }
 
 $("#renderBtn").click(
